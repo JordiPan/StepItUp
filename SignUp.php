7851c4d49382dc0bec88da650b27c50b1070051b
JordiPan <jordipan.jp@gmail.com>
2022-12-24 19:56:21 +0100

    changed db queries for signup

--- SignUp.php
+++ SignUp.php
@@ -2,13 +2,14 @@
 require "Database.php";
 $db = new Database();
 
-if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['repeatPassword'])){
+if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['repeatPassword']) && isset($_POST['email'])){
    $username = $_POST['username'];
     $password = $_POST['password'];
     $repeat = $_POST['repeatPassword'];
+    $email = $_POST['email'];
     if ($password == $repeat){
         if($db->dbConnect()) {
-            if($db->signUp("gebruiker",$username, $password)) {
+            if($db->signUp("gebruiker",$username, $email, $password)) {
                 echo "Success!";
             }else echo "gebruikersnaam wordt al gebruikt"; //kan ook DB fout zijn (niet waarschijnlijk)
         }else echo "Geen connectie met database...";
